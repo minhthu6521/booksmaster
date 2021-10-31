9389027267bda47031e5f3c993d968b932ed86af
Thu Nguyen <minhthu6521@gmail.com>
2021-10-31 21:37:40 +0200

    more metadata uploaded

--- books_backend/scripts/add_book.py
+++ books_backend/scripts/add_book.py
@@ -5,9 +5,13 @@ import sys
 from database import engine
 from elasticsearch import Elasticsearch
 from models.author import AuthorORM
+import xml.etree.ElementTree as ET
 from models.book import BookORM
+from models.book import GenreORM
 from scripts.read_epub import epubtohtml
 from sqlalchemy.orm import Session
+
+from scripts.read_epub import split_author
 
 es = Elasticsearch(
     [os.getenv("ELASTIC_SEARCH_URL") or "localhost"],
@@ -24,16 +28,44 @@ def get_book():
         print(hit["_id"])
 
 
-def add_book(path):
+def get_metadata_from_file(metadata_file):
+    tree = ET.parse(metadata_file)
+    result = {
+    }
+    elements = tree.find('{http://www.idpf.org/2007/opf}metadata')
+    for el in elements:
+        if "title" in el.tag:
+            result["title"] = el.text
+        elif "creator" in el.tag:
+            result.setdefault("author", [])
+            result["author"].append(split_author(el.text))
+        elif "language" in el.tag:
+            result["language"] = el.text
+        elif "subject" in el.tag:
+            result.setdefault("tags", [])
+            result["tags"].append(el.text)
+        elif "description" in el.tag:
+            result["description"] = el.text
+        elif "identifier" in el.tag and el.attrib.get("{http://www.idpf.org/2007/opf}scheme") == "ISBN":
+            result["isbn"] = el.text
+    return result
+
+
+def add_book(path, metadata_file=None):
     book_content = epubtohtml(path)
     if not book_content:
         return
+    if metadata_file:
+        metadata = get_metadata_from_file(metadata_file)
+        book_content.update(metadata)
     with Session(engine) as session:
         session.expire_on_commit = False
         authors = book_content.pop("author")
+        tags = book_content["tags"]
         book = BookORM(title=book_content.get("title", ""),
                        isbn=book_content.get("isbn", ""),
-                       language=book_content.get("language", "en"))
+                       language=book_content.get("language", "en"),
+                       description=book_content.get("description", ""))
         for au in authors:
             author = session.query(AuthorORM).filter(AuthorORM.first_name == au["first_name"],
                                                      AuthorORM.last_name == au["last_name"]).first()
@@ -42,6 +74,14 @@ def add_book(path):
                 session.add(author)
                 session.flush()
             book.authors.append(author)
+        if tags:
+            for tag in tags:
+                genre = session.query(GenreORM).filter(GenreORM.name == tag).first()
+                if not genre:
+                    genre = GenreORM(name=tag)
+                    session.add(genre)
+                    session.flush()
+                book.genres.append(genre)
         session.add(book)
         session.commit()
         book_content["external_id"] = book.id
@@ -54,7 +94,7 @@ def add_all_books_in_dir(dir, action):
     for root, dirs, files in os.walk(dir):
         for file in files:
             if file.endswith(".epub"):
-                action(os.path.join(root, file))
+                action(os.path.join(root, file), metadata_file=os.path.join(root, "metadata.opf"))
 
 
 def delete_books():
@@ -72,4 +112,5 @@ def delete_books():
 
 if __name__ == '__main__':
     path = sys.argv[1]
-    add_book(path)
+    delete_books()
+    add_all_books_in_dir(path, add_book)
--- books_backend/scripts/read_epub.py
+++ books_backend/scripts/read_epub.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from ebooklib import ITEM_DOCUMENT
 from ebooklib import epub
+from nltk.corpus import wordnet
 
 NOT_READ_TAGS = ['[document]', 'noscript', 'header', 'html', 'meta', 'head', 'input', 'script', 'style']
 
@@ -21,6 +22,31 @@ def html2text(html, not_read=NOT_READ_TAGS):
     return output
 
 
+def is_valid_eng_tags(tag):
+    if not tag:
+        return
+    tag = tag.title().strip()
+    tag_child = tag.split(" ")
+    for t in tag_child:
+        if not wordnet.synsets(t):
+            return
+    return tag
+
+
+def split_author(author):
+    if "," not in author:
+        author_to_json = {
+            "first_name": " ".join(author.split(" ")[:-1]),
+            "last_name": author.split(" ")[-1]
+        }
+    else:
+        author_to_json = {
+            "first_name": author.split(",")[1],
+            "last_name": author.split(",")[0]
+        }
+    return author_to_json
+
+
 def epubtohtml(path):
     try:
         book = epub.read_epub(path)
@@ -29,20 +55,22 @@ def epubtohtml(path):
     read_metadata = lambda x, y: strip_metadata(book.get_metadata(x, y))
     title = read_metadata('DC', 'title')
     authors = read_metadata('DC', 'creator')
-    authors = authors.split(";")
+    if ";" in authors:
+        authors = authors.split(";")
+    else:
+        authors = authors.split("&")
+    _raw_tags = book.get_metadata('DC', 'subject')
+    tags = None
+    if _raw_tags:
+        if not isinstance(_raw_tags, list):
+            _raw_tags = _raw_tags.split("/")
+        else:
+            _raw_tags = [t[0] for t in _raw_tags]
+        tags = [is_valid_eng_tags(t) for t in _raw_tags] if _raw_tags else None
+        tags = [t for t in tags if t]
     authors_json = []
     for author in authors:
-        if "," not in author:
-            author_to_json = {
-                "first_name": " ".join(author.split(" ")[:-1]),
-                "last_name": author.split(" ")[-1]
-            }
-        else:
-            author_to_json = {
-                "first_name": author.split(",")[1],
-                "last_name": author.split(",")[0]
-            }
-        authors_json.append(author_to_json)
+        authors_json.append(split_author(author))
     isbn = read_metadata('DC', 'identifier')
     language = read_metadata('DC', 'language') or "en"
     description = html2text(read_metadata('DC', 'description'))
@@ -57,7 +85,8 @@ def epubtohtml(path):
         "language": language,
         "description": description,
         "content": content_text,
-        "html": content_html
+        "html": content_html,
+        "tags": tags
     }
 
 
